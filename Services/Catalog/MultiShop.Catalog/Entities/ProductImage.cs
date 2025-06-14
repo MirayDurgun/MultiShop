978224cb5928ac496b12133d340e0f6b2035deb3
miray <miray@MIRAY>
2025-06-14 11:00:34 +0300

    9. ders sonu

--- Services/Catalog/MultiShop.Catalog/Entities/ProductImage.cs
+++ Services/Catalog/MultiShop.Catalog/Entities/ProductImage.cs
@@ -3,7 +3,7 @@ using MongoDB.Bson.Serialization.Attributes;
 
 namespace MultiShop.Catalog.Entities
 {
-    public class ProductImages
+    public class ProductImage
     {
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
