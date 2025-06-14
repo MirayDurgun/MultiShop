aaea0a5de73bbd151c63ff5f644b357981440aba
miray <miraydurgun67@gmail.com>
2025-06-14 11:19:00 +0300

    11. ders sonu

---
+++ Services/Catalog/MultiShop.Catalog/Dtos/CategoryDtos/ResultCategoryDto.cs
@@ -0,0 +1,9 @@
+﻿namespace MultiShop.Catalog.Dtos.CategoryDtos
+{
+    public class ResultCategoryDto
+    {
+        //listelemek istediğim kategorilerim. Her bir Crud için olacak
+        public string CategoryId { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
