9f4ad7ba34a69078d91b7ff51c468a5b3280bc6a
miray <miraydurgun67@gmail.com>
2025-06-14 11:38:00 +0300

    17. ders sonu

--- Services/Catalog/MultiShop.Catalog/Services/CategoryServices/CategoryService.cs
+++ Services/Catalog/MultiShop.Catalog/Services/CategoryServices/CategoryService.cs
@@ -2,6 +2,7 @@
 using MongoDB.Driver;
 using MultiShop.Catalog.Dtos.CategoryDtos;
 using MultiShop.Catalog.Entities;
+using MultiShop.Catalog.Settings;
 
 namespace MultiShop.Catalog.Services.CategoryServices
 {
@@ -14,9 +15,29 @@ namespace MultiShop.Catalog.Services.CategoryServices
         private readonly IMongoCollection<Category> _categoryCollection;
         private readonly IMapper _mapper;
 
-        public Task CreateCategoryAsync(CreateCategoryDto createCategoryDto)
+        public CategoryService(IMapper mapper, IDatabaseSettings _databaseSettings)
         {
-            throw new NotImplementedException();
+            // 3 aşama vardır. 1.Bağlantı 2.Databse, 3.Tablo
+            //1. Bağlantı
+            var client = new MongoClient(_databaseSettings.ConnectionStrings); //connaction stringse erişim sağlıyoruz
+                                                                               //MongoDB'ye bağlanır.
+
+            //2. Databse
+            var database = client.GetDatabase(_databaseSettings.DatabaseName); //client.GetDatabase veri tabanına erişim sağlar
+                                                                               //Bağlanılacak veritabanının adını içerir
+
+            //3. Tablo
+            _categoryCollection = database.GetCollection<Category>(_databaseSettings.CategoryCollectionName);
+            // Category koleksiyonuna erişim sağlar.
+
+            _mapper = mapper;
+        }
+
+        public async Task CreateCategoryAsync(CreateCategoryDto createCategoryDto)
+        {
+            var value = _mapper.Map<Category>(createCategoryDto);
+           // await
+            
         }
 
         public Task DeleteCategoryAsync(string id)
