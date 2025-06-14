289ac13462560cc581f4796d24b3de011e02c299
miray <miray@MIRAY>
2025-06-14 10:54:18 +0300

    7. Ders sonu

--- Services/Catalog/MultiShop.Catalog/Entities/Category.cs
+++ Services/Catalog/MultiShop.Catalog/Entities/Category.cs
@@ -1,6 +1,15 @@
-﻿namespace MultiShop.Catalog.Entities
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MultiShop.Catalog.Entities
 {
-	public class Category
-	{
-	}
+    public class Category
+    {
+        [BsonId] // MongoDB'de bu alanın belge kimliği (Id) olduğunu belirtir.
+
+        [BsonRepresentation(BsonType.ObjectId)] // MongoDB'de ObjectId olarak saklanır, C# tarafında string olarak kullanılır.
+
+        public string CategoryId { get; set; }
+        public string CategoryName { get; set; }
+    }
 }
