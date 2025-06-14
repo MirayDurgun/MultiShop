d17f1be2432ea153db78482e4b6d626612ad9bb8
miray <miray@MIRAY>
2025-06-14 11:07:14 +0300

    10. ders sonu

---
+++ Services/Catalog/MultiShop.Catalog/Settings/IDatabaseSettings.cs
@@ -0,0 +1,14 @@
+﻿namespace MultiShop.Catalog.Settings
+{
+    public interface IDatabaseSettings
+    {
+        //MongoDB ile bağlantı için gerekli olan ayarları içerecek sınıfların uygulaması gereken bir yapıdır.
+        //CRUD işlemleri için gerekli
+        public string CategoryCollectionName { get; set; }
+        public string ProductCollectionName { get; set; }
+        public string ProductDetailCollectionName { get; set; }
+        public string ProductImageCollectionName { get; set; }
+        public string ConnectionStrings { get; set; }
+        public string DatabaseName { get; set; }
+    }
+}
