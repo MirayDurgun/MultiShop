82b4527d517224f89a988db0c7c1a778627ef710
miray <miraydurgun67@gmail.com>
2025-06-14 11:28:35 +0300

    14. ders sonu

---
+++ Services/Catalog/MultiShop.Catalog/Mapping/GeneralMapping.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using MultiShop.Catalog.Dtos.CategoryDtos;
+using MultiShop.Catalog.Dtos.ProductDetailDtos;
+using MultiShop.Catalog.Dtos.ProductDtos;
+using MultiShop.Catalog.Dtos.ProductImageDtos;
+using MultiShop.Catalog.Entities;
+
+namespace MultiShop.Catalog.Mapping
+{
+    public class GeneralMapping :Profile
+    {
+        public GeneralMapping()
+        {
+            // Mapping, bir nesneyi başka bir nesneye dönüştürmek için kullanılır.
+            // Örnek: Entity -> DTO veya DTO -> Entity dönüşümü.
+
+
+            CreateMap<Category, ResultCategoryDto>().ReverseMap(); //categori sınıfı ile dto maple eşle
+            CreateMap<Category, CreateCategoryDto>().ReverseMap();
+            CreateMap<Category, UpdateCategoryDto>().ReverseMap();
+            CreateMap<Category, GetByIdCategoryDto>().ReverseMap();
+
+            CreateMap<Product, ResultProductDto>().ReverseMap();
+            CreateMap<Product, CreateProductDto>().ReverseMap();
+            CreateMap<Product, UpdateProductDto>().ReverseMap();
+            CreateMap<Product, GetByIdProductDto>().ReverseMap();
+
+            CreateMap<ProductDetail, ResultProductDetailDto>().ReverseMap();
+            CreateMap<ProductDetail, CreateProductDetailDto>().ReverseMap();
+            CreateMap<ProductDetail, UpdateProductDetailDto>().ReverseMap();
+            CreateMap<ProductDetail, GetByIdProductDetailDto>().ReverseMap();
+
+            CreateMap<ProductsImage, ResultProductImageDto>().ReverseMap();
+            CreateMap<ProductsImage, CreateProducImageDto>().ReverseMap();
+            CreateMap<ProductsImage, UpdateProductImageDto>().ReverseMap();
+            CreateMap<ProductsImage, GetByIdProductImageDto>().ReverseMap();
+        }
+    }
+}
