c63a6635954fc351431bec953b0c855472a53df0
miray <miraydurgun67@gmail.com>
2025-06-14 11:34:11 +0300

    15. ders sonu

---
+++ Services/Catalog/MultiShop.Catalog/Services/CategoryServices/CategoryService.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using MongoDB.Driver;
+using MultiShop.Catalog.Dtos.CategoryDtos;
+using MultiShop.Catalog.Entities;
+
+namespace MultiShop.Catalog.Services.CategoryServices
+{
+    public class CategoryService : ICategoryService
+    {
+        //MongoDB koleksiyonlarını temsil eden bir arayüzdür
+        //yapı sayesinde belirli bir koleksiyondaki (tablodaki) belgelere (documents) erişim sağlanır.
+        //Veritabanıyla CRUD işlemleri (Create, Read, Update, Delete) yapmak için temel bir araçtır.
+        //MongoDB'de Category adında bir koleksiyona (tabloya) erişmek için tanımlanmıştır
+        private readonly IMongoCollection<Category> _categoryCollection;
+        private readonly IMapper _mapper;
+
+        public Task CreateCategoryAsync(CreateCategoryDto createCategoryDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteCategoryAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<ResultCategoryDto>> GetAllCategoryAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<GetByIdCategoryDto> GetByIdCategoryAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateCategoryAsync(UpdateCategoryDto updateCategoryDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Services/Catalog/MultiShop.Catalog/Services/CategoryServices/ICategoryService.cs
@@ -0,0 +1,15 @@
+﻿using MultiShop.Catalog.Dtos.CategoryDtos;
+
+namespace MultiShop.Catalog.Services.CategoryServices
+{
+    public interface ICategoryService
+    {
+        //kategorilerle ilgili CRUD işlemleri (Oluşturma, Okuma, Güncelleme, Silme) yapmak için asenkron yöntemler içeriyor
+        // Task -> Asenkron olarak çalışır ve işlem tamamlandığında herhangi bir veri döndürmez (void)
+        Task<List<ResultCategoryDto>> GetAllCategoryAsync();
+        Task CreateCategoryAsync(CreateCategoryDto createCategoryDto);
+        Task UpdateCategoryAsync(UpdateCategoryDto updateCategoryDto);
+        Task DeleteCategoryAsync(string id);
+        Task<GetByIdCategoryDto> GetByIdCategoryAsync(string id);
+    }
+}
