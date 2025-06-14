de3033d88f9a6e006a25e9203871cb68e252cf49
miray <miray@MIRAY>
2025-06-14 10:57:23 +0300

    8. ders sonu

--- Services/Catalog/MultiShop.Catalog/Entities/ProductDetail.cs
+++ Services/Catalog/MultiShop.Catalog/Entities/ProductDetail.cs
@@ -1,6 +1,19 @@
-﻿namespace MultiShop.Catalog.Entities
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MultiShop.Catalog.Entities
 {
-    public class ProductDeta
+    public class ProductDetail
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string ProductDetailID { get; set; }
+        public string ProductDetailDescription { get; set; }
+        public string ProductDetailInfo { get; set; }
+        public string ProdutcId { get; set; }
+
+		[BsonIgnore]
+		public Product Product { get; set; }
+        
     }
 }
